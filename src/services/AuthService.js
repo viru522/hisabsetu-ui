bcfe668364f7bba5f4f16b56a71b2617509d771e
virendra-kanojiya <56133577+viru522@users.noreply.github.com>
2026-04-06 09:37:27 +0530

    Refactor comments in AuthService.js

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -1,26 +1,26 @@
 import API from "./Api";
 import { jwtDecode } from "jwt-decode";
 
-// 🔥 LOGIN
-export const login = async (data) => {
+//  LOGIN
+  export const login = async (data) => {
   const res = await API.post("/auth/login", data);
 
   const accessToken = res.data.accessToken;
   const refreshToken = res.data.refreshToken;
 
-  // 🔥 STORE TOKENS
+  //  STORE TOKENS
   localStorage.setItem("accessToken", accessToken);
   localStorage.setItem("refreshToken", refreshToken);
 
-  // 🔥 STORE ROLE
+  // STORE ROLE
   const decoded = jwtDecode(accessToken);
   localStorage.setItem("role", decoded.role);
 
   return res.data;
 };
 
-// 🔥 LOGOUT
-export const logout = () => {
+// LOGOUT
+  export const logout = () => {
   localStorage.removeItem("accessToken");
   localStorage.removeItem("refreshToken");
   localStorage.removeItem("role");
